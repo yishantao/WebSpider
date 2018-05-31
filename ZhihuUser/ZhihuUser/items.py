e679417c7fa0d366c030b42ce488ea3fa35b385a
yishantao <38241025+yishantao@users.noreply.github.com>
2018-05-31 13:01:50 +0800

    feat: add item

--- ZhihuUser/ZhihuUser/items.py
+++ ZhihuUser/ZhihuUser/items.py
@@ -5,10 +5,30 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class ZhihuuserItem(scrapy.Item):
+# 默认Item名可以改
+class UserItem(Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    allow_message = Field()
+    answer_count = Field()
+    articles_count = Field()
+    avatar_url = Field()
+    avatar_url_template = Field()
+    badge = Field()
+    employments = Field()
+    follower_count = Field()
+    gender = Field()
+    headline = Field()
+    id = Field()
+    is_advertiser = Field()
+    is_blocking = Field()
+    is_followed = Field()
+    is_following = Field()
+    is_org = Field()
+    name = Field()
+    type = Field()
+    url = Field()
+    url_token = Field()
+    user_type = Field()
